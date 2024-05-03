07afba8332f21d4b26d6d1438f9aa3831f1d1a63
Daniel Portoghese <daniel.portoghese@agencianuts.com.br>
2024-05-03 17:52:07 -0300

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,22 @@
 <template>
-  <header class="bg-gray-800 text-white">
-    <div class="flex justify-between items-center py-4 px-8">
-      <div class="text-xl font-bold">Logo Aqui</div>
-      <nav>
-        <div class="text-2xl cursor-pointer" @click="toggleMenu">&#9776;</div>
-        <ul class="menu flex items-center space-x-4" :class="{ 'hidden': !isMenuOpen }">
-          <li class="primary-item">Bem-vindo. Entre ou cadastre-se</li>
-          <li>Home</li>
-          <li>Sobre Nós</li>
-          <li>Faq</li>
-        </ul>
-      </nav>
+  <header class="bg-gray-800 text-white fixed w-full top-0 z-10">
+    <div class="flex w-full justify-between items-center py-4 px-8">
+      <div class="text-xl font-bold cursor-pointer">
+        Logo Aqui
+      </div>
+      <div class="text-2xl cursor-pointer" @click="toggleMenu">&#9776;</div>
+
     </div>
   </header>
+  <nav>
+    <ul class="menu flex flex-col absolute top-0 right-0 w-full h-full bg-gray-800 text-white space-y-4 p-8" :class="{ 'hidden':!isMenuOpen }">
+      <li class="primary-item">Bem-vindo. Entre ou cadastre-se</li>
+      <li>Home</li>
+      <li>Sobre Nós</li>
+      <li>Serviços</li>
+      <li>Contato</li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
@@ -21,6 +25,12 @@ import { ref } from 'vue'
 const isMenuOpen = ref(false)
 
 const toggleMenu = () => {
-  isMenuOpen.value = !isMenuOpen.value
+  isMenuOpen.value =!isMenuOpen.value
 }
 </script>
+
+<style scoped>
+.menu {
+  transition: all 0.3s ease-out;
+}
+</style>
